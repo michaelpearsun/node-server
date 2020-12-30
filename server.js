3f7e951c7c9d65ab85ced795e1d480e1867f0425
Michael Pearson <michaelpearsun@gmail.com>
2020-12-29 21:02:28 -0600

    Node HTTP module part 2

--- server.js
+++ server.js
@@ -1,13 +1,44 @@
+const fs = require('fs');
 const http = require('http');
+const path = require('path');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Hello WOrld!<h1><body><html>')
+    console.log(`Request for ${req.url} by method ${req.method}`);
+
+    if (req.method === 'GET') {
+        let fileUrl = req.url;
+        if (fileUrl ==='/') {
+            fileUrl = '/index.html';
+        }
+
+        const filePath = path.resolve('./public' + fileUrl);
+        const fileExt = path.extname(filePath);
+        if (fileExt === '.html') {
+            fs.access(filePath, err => {
+                if (err) {
+                    res.status = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(`<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`);
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+
+                fs.createReadStream(filePath).pipe(res)
+            })
+        } else {
+            res.status = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(`<html><body><h1>Error 404: ${fileUrl} is not an HTML file</h1></body></html>`);
+        }
+    } else {
+        res.status = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(`<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>`);
+    }
 });
 
 server.listen(port, hostname, () => {
